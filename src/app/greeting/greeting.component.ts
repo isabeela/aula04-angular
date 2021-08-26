f63abf1340bc113ad0a9baec2ee1cedb8029a778
isabeela <isabeelabg@gmail.com>
2021-08-26 18:10:53 -0300

    2 files modified

--- src/app/greeting/greeting.component.ts
+++ src/app/greeting/greeting.component.ts
@@ -16,4 +16,20 @@ export class GreetingComponent implements OnInit {
   getHour() {
     return this.timestamp.getHours();
   }
+
+  isDawn() {
+    return this.getHour() < 12;
+  }
+
+  isMorning() {
+    return this.getHour() >= 6 && this.getHour() < 12;
+  }
+
+  isAfternoon() {
+    return this.getHour() >= 12 && this.getHour() < 18;
+  }
+
+  isNight() {
+    return this.getHour() >= 18 && this.getHour() < 24;
+  }
 }
